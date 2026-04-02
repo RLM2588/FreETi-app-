4b886359998e7340c300f3c2c12e632fd435f0b5
Etiwh <etiwh537@gmail.com>
2026-04-02 23:46:35 +0300

    Савков Всеволод. изменил файловую систему и проверил работу способность

--- app/src/main/java/com/example/freeti/MainActivity.kt
+++ app/src/main/java/com/example/freeti/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.freeti
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main_screen : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        main_screen = findViewById(R.id.main_screen_text_button)
+        main_screen.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
