9bdee8ef3a431f8c8ae08da71effdcddefbc4326
Radiy_Aseev <radiyaseev@gmail.com>
2026-04-20 21:11:55 +0300

    Сделал тестовые функции на мейн активити и функции для теста сетевого взаимодействия

---
+++ app/src/main/java/com/example/freeti/repository/TestRepository.kt
@@ -0,0 +1,23 @@
+package com.example.freeti.repository
+
+import com.example.freeti.network_api.ApiService
+import com.example.freeti.network_entity.TestRequest
+
+class TestRepository(
+    private val api: ApiService
+) {
+    suspend fun send(out_text: String): Result<String> {
+        return try {
+            val response = api.test(TestRequest(out_text))
+            if (response.isSuccessful) {
+                response.body()?.let { testResponse ->
+                    Result.success(testResponse.input_text)
+                } ?: Result.failure(Exception("Empty response"))
+            } else {
+                Result.failure(Exception("test failed"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
