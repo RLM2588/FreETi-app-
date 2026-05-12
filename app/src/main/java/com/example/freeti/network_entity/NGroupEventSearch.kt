4b8b3f1cc6aa7d65a3c4b297937ef2cc02466a00
Aseychik <aseev.egor45@yandex.ru>
2026-05-12 22:57:16 +0300

    added user endpoints, but not checked yet

---
+++ app/src/main/java/com/example/freeti/network_entity/NGroupEventSearch.kt
@@ -0,0 +1,12 @@
+package com.example.freeti.network_entity
+
+data class NGroupEventSearch (
+    val day_start: String,
+    val day_end: String,
+    val time_start: String,
+    val time_end: String,
+    val importance: Int,
+    val title: String,
+    val body: String,
+    val colour: String = "FFFFFF"
+)
--- app/src/main/java/com/example/freeti/network_entity/NGroupsUsers.kt
+++ app/src/main/java/com/example/freeti/network_entity/NGroupsUsers.kt
@@ -1,7 +1,15 @@
 package com.example.freeti.network_entity
 
+import com.example.freeti.data.local.entity.DGroupsUsers
+
 data class NGroupsUsers (
     val group: String,
     val user1: Int,
     val role: String //TODO String enum??
-)
+) {
+    fun toEntity() : DGroupsUsers = DGroupsUsers(
+        tgroup = this.group,
+        user1 = this.user1,
+        role = this.role
+    )
+}
