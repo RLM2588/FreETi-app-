9a785a0083a2c277903b8cdd387576893ea1e9a1
Aseychik <aseev.egor45@yandex.ru>
2026-05-14 20:48:05 +0300

    add log out button

--- app/src/main/java/com/example/freeti/SettingsActivity.kt
+++ app/src/main/java/com/example/freeti/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.freeti
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
@@ -15,6 +16,8 @@ class SettingsActivity : AppCompatActivity() {
     private lateinit var s_esc: Button
     private lateinit var pref: SharedPreferences
 
+    private lateinit var logOutButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +30,7 @@ class SettingsActivity : AppCompatActivity() {
         s_check = findViewById(R.id.settings_test)
         s_auto = findViewById(R.id.settings_auto)
         s_esc = findViewById(R.id.settings_esc)
+        logOutButton = findViewById(R.id.logOutButton)
 
         pref = getSharedPreferences("settings", MODE_PRIVATE)
 
@@ -38,5 +42,13 @@ class SettingsActivity : AppCompatActivity() {
         s_auto.setOnCheckedChangeListener { _, _ -> pref.edit().putBoolean("AutoToMain", s_auto.isChecked).apply() }
 
         s_esc.setOnClickListener { finish() }
+        logOutButton.setOnClickListener {
+            (application as MyApp).appContainer.tokenManager.clearTokens()
+            startActivity(Intent(this@SettingsActivity, LoginActivity::class.java))
+            finish()
+        }
     }
+
+
+
 }
